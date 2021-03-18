ebfc4e0e3f398f2cb1688f11e1330fb4a74a2f68
Henrik Burmann <henriabu@stud.ntnu.no>
2021-03-18 12:53:40 +0100

    Created classes Task and Category

--- src/main/java/Rescource/Task.java
+++ src/main/java/Rescource/Task.java
@@ -1,5 +1,10 @@
 package Rescource;
 
-public class Task {
+import java.time.LocalDateTime;
 
+public class Task {
+    private String TaskName;
+    private LocalDateTime time;
+    private String priority;
+    private Category category;
 }
