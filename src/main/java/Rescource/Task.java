5d5c5becb2ab62fd6acd4875f144a07a6412fcc0
Henrik Burmann <henriabu@stud.ntnu.no>
2021-03-18 13:07:53 +0100

    Changed .gitignore

--- src/main/java/Rescource/Task.java
+++ src/main/java/Rescource/Task.java
@@ -4,7 +4,23 @@ import java.time.LocalDateTime;
 
 public class Task {
     private String TaskName;
-    private LocalDateTime time;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private String priority;
     private Category category;
+    private String description;
+
+    public Task(String taskName, LocalDateTime startTime, LocalDateTime endTime, String priority,
+                Category category, String description){
+        if (taskName == null || taskName.equals("")){
+            throw new IllegalArgumentException("Task must have a name");
+        }
+        if (startTime.equals("")){
+
+        }
+
+    }
+
+    public Task(String taskName, LocalDateTime startTime, LocalDateTime endTime, String priority,
+                Category category){}
 }
