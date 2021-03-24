4d428928c8f63f9ff55964e19c703dbe00bda1f2
Martin Helmersen <Marthelm@stud.ntnu.no>
2021-03-24 18:13:52 +0100

    Started anew from archetype javafx

---
+++ src/main/java/idatt1002_2021_k1-08/SecondaryController.java
@@ -0,0 +1,12 @@
+package idatt1002_2021_k1-08;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module idatt1002_2021_k1-08 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens idatt1002_2021_k1-08 to javafx.fxml;
+    exports idatt1002_2021_k1-08;
+}
