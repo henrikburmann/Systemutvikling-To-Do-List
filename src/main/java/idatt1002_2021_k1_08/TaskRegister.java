25c9764b2cd38623bda11dcb1408de0640625c5a
Martin Helmersen <Marthelm@stud.ntnu.no>
2021-03-25 16:11:42 +0100

    Changed from ArrayList to ObservableList in Task

--- src/main/java/idatt1002_2021_k1_08/TaskRegister.java
+++ src/main/java/idatt1002_2021_k1_08/TaskRegister.java
@@ -1,19 +1,188 @@
 package idatt1002_2021_k1_08;
 
-import java.util.ArrayList;
+import javafx.beans.InvalidationListener;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+
+import java.util.*;
 
 public class TaskRegister {
 
-    private ArrayList<Task> tasks;
+    private ObservableList<Task> tasks;
 
-    public TaskRegister(ArrayList<Task> tasks){
+    public TaskRegister(ObservableList<Task> tasks){
         if(tasks.isEmpty()){
             System.out.println("No tasks inside task list");
         }
-        this.tasks = new ArrayList<>();
+        this.tasks = new ObservableList<Task>() {
+            @Override
+            public void addListener(ListChangeListener<? super Task> listChangeListener) {
+
+            }
+
+            @Override
+            public void removeListener(ListChangeListener<? super Task> listChangeListener) {
+
+            }
+
+            @Override
+            public boolean addAll(Task... tasks) {
+                return false;
+            }
+
+            @Override
+            public boolean setAll(Task... tasks) {
+                return false;
+            }
+
+            @Override
+            public boolean setAll(Collection<? extends Task> collection) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Task... tasks) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Task... tasks) {
+                return false;
+            }
+
+            @Override
+            public void remove(int i, int i1) {
+
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<Task> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Task task) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends Task> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, Collection<? extends Task> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Task get(int index) {
+                return null;
+            }
+
+            @Override
+            public Task set(int index, Task element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, Task element) {
+
+            }
+
+            @Override
+            public Task remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Task> listIterator() {
+                return null;
+            }
+
+            @Override
+            public ListIterator<Task> listIterator(int index) {
+                return null;
+            }
+
+            @Override
+            public List<Task> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+
+            @Override
+            public void addListener(InvalidationListener invalidationListener) {
+
+            }
+
+            @Override
+            public void removeListener(InvalidationListener invalidationListener) {
+
+            }
+        };
     }
 
-    public ArrayList<Task> getTasks() {
+    public ObservableList<Task> getTasks() {
         if(tasks.isEmpty()){
             System.out.println("No tasks inside task list");
         }
