27b6b93d09d7be2d45c7c3b93f922f52cd0f0a3f
Henrik Burmann <henriabu@stud.ntnu.no>
2021-03-27 14:02:07 +0100

    Made a draft for classes Category og CategoryRegister

--- src/main/java/idatt1002_2021_k1_08/Category.java
+++ src/main/java/idatt1002_2021_k1_08/Category.java
@@ -1,4 +1,55 @@
 package idatt1002_2021_k1_08;
 
+import java.util.ArrayList;
+
 public class Category {
+    private String name;
+    private ArrayList<Task> tasks;
+
+    public Category(String name){
+        if (name == null || name.equals("")){
+            throw new IllegalArgumentException("Category must have a name");
+        }
+        this.name = name;
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task){
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task){
+        tasks.remove(task);
+    }
+
+    public String getName(){
+        return name;
+    }
+    public ArrayList<Task> getTasks(){
+        return tasks;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o){
+            return true;
+        }
+        else if (!(o instanceof Category)){
+            return false;
+        }
+        Category category = (Category) o;
+        return category.getName().equals(this.getName());
+    }
+
+    public String toString(){
+        StringBuilder text = new StringBuilder();
+        text.append(name + ":\n");
+        tasks.forEach(e -> text.append(e.toString()));
+        text.append("\n");
+        return text.toString();
+    }
 }
