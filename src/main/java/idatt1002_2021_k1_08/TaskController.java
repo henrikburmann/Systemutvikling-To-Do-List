e29b2bdbc45e595370931ae29db922cb767a65a0
trygveam <trygveamundsen@live.no>
2021-04-04 19:32:33 +0200

    added a few changes before meeting

--- src/main/java/idatt1002_2021_k1_08/TaskController.java
+++ src/main/java/idatt1002_2021_k1_08/TaskController.java
@@ -1,7 +1,7 @@
 package idatt1002_2021_k1_08;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -10,6 +10,33 @@ public class TaskController {
 
     @FXML
     Button add_task_button;
+    @FXML
+    Button edit_task_button;
+    @FXML
+    Button delete_task_button;
+
+    @FXML
+    Label task_information_label;
+
+    @FXML
+    Button add_task_complete_button;
+
+    @FXML
+    ListView task_list;
+
+    @FXML
+    TextField task_name_textfield;
+    @FXML
+    DatePicker date_time_box;
+    @FXML
+    ChoiceBox category_list;
+    @FXML
+    TextArea notes_textarea;
+
+
+
+
+
 
     @FXML
     public void changeSceneToAddTask() throws IOException{
