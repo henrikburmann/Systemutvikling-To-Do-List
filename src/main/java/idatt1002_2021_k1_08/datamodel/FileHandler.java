fa04e2acf650f6623fb36178e4b4e30a5ab27ecb
Marcus Johannessen <marcusjo@stud.ntnu.no>
2021-04-13 18:34:11 +0200

    Deleted unused methods

--- src/main/java/idatt1002_2021_k1_08/datamodel/FileHandler.java
+++ src/main/java/idatt1002_2021_k1_08/datamodel/FileHandler.java
@@ -46,58 +46,11 @@ public class FileHandler {
     /**
      * Writes a Category to file
      */
-    private void serializeCategory(ArrayList<Task> categories) throws IOException{
+    private void serializeTask(ArrayList<Task> categories) throws IOException{
         try(FileOutputStream fs = new FileOutputStream(FILE_PATH); //åpner opp en stream
             ObjectOutputStream os = new ObjectOutputStream(fs)){
             os.writeObject(categories);
         }
-    }
-
-    /**
-     * Reads a category from a file
-     */
-    public ArrayList<Category> deserializeCategory() throws IOException {
-        ArrayList<Category> category = new ArrayList<>();
-
-        try (FileInputStream fs = new FileInputStream(FILE_PATH);
-             ObjectInputStream is = new ObjectInputStream(fs)) {
-
-            category = (ArrayList<Category>) is.readObject();
-        }catch (ClassNotFoundException ex){
-            ex.printStackTrace();
-        }
-        return category;
-    }
-
-    /**
-     *
-     * @param ob
-     * @throws IOException
-     */
-    public void serializeTask(Object ob) throws IOException{
-
-        //Path path = Paths.get(filename);
-        try (FileOutputStream fs = new FileOutputStream(FILE_PATH);
-            ObjectOutputStream os = new ObjectOutputStream(fs)){
-            os.writeObject(ob);
-        }
-    }
-
-    /**
-     *
-     * @return Object representing a task
-     * @throws IOException
-     */
-    public Object deserializeObject() throws IOException{
-        Object task = null;
-        try(FileInputStream fs = new FileInputStream(FILE_PATH);
-            ObjectInputStream is = new ObjectInputStream(fs)){
-            task = is.readObject();
-
-        }catch (ClassNotFoundException ex){
-            ex.printStackTrace();
-        }
-        return task;
     }
 
      private ArrayList<Task> deserializeTask() throws IOException {
@@ -107,20 +60,21 @@ public class FileHandler {
               ObjectInputStream is = new ObjectInputStream(fs)) {
 
               tasks1 = (ArrayList<Task>) is.readObject();
+
          }catch (ClassNotFoundException ex){
              ex.printStackTrace();
          }
          return tasks1;
      }
 
-
+     //TODO: Denne funker ikke som det skal så må fikse på den
      public void storeData() throws IOException{
         ArrayList<Task> store = new ArrayList<>();
         for (Task t: tasks){
             Task task = new Task(t.getTaskName(),t.getDescription(),t.getStartDate(),t.getEndDate(),t.getPriority());
             store.add(task);
         }
-        serializeCategory(store);
+        serializeTask(store);
      }
 
      public void loadData() throws IOException{
