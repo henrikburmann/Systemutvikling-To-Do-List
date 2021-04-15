8cc2592a033657d722815711122c303949903adb
Marcus Johannessen <marcusjo@stud.ntnu.no>
2021-04-15 15:03:04 +0200

    Fixed sorting list problem

--- src/main/java/idatt1002_2021_k1_08/TaskController.java
+++ src/main/java/idatt1002_2021_k1_08/TaskController.java
@@ -5,17 +5,13 @@ import idatt1002_2021_k1_08.datamodel.Task;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.FileInputStream;
@@ -24,7 +20,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Optional;
-import java.util.function.Predicate;
 
 public class TaskController {
 
@@ -61,12 +56,18 @@ public class TaskController {
     @FXML
     ComboBox<String> categoryList;
 
-    @FXML TextField taskNameTextField;
-    @FXML TextField endDateTextField;
-    @FXML TextField startDateTextField;
-    @FXML TextField priorityTextField;
-    @FXML TextField categoryTextField;
-    @FXML TextArea notesTextArea;
+    @FXML
+    TextField taskNameTextField;
+    @FXML
+    TextField endDateTextField;
+    @FXML
+    TextField startDateTextField;
+    @FXML
+    TextField priorityTextField;
+    @FXML
+    TextField categoryTextField;
+    @FXML
+    TextArea notesTextArea;
     ArrayList<TextField> textfieldList = new ArrayList<>();
 
 
@@ -83,14 +84,12 @@ public class TaskController {
         menuButton = new MenuButton("Options", null, helpItem);
         categoryList.setItems(FileHandler.getCategories());
         //TODO: look at filtered list and sorted list, for displaying tasks by category...
-        tasksView.setItems(sortedList);
-        tasksView.getSelectionModel().selectFirst();
         //Show information of task in description area
         //Also implements listener for every Task
         tasksView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Task>() {
             @Override
             public void changed(ObservableValue<? extends Task> observableValue, Task task, Task t1) {
-                if(t1 != null){
+                if (t1 != null) {
 
                     Task task1 = tasksView.getSelectionModel().getSelectedItem();
                     fillInformationArea(task1);
@@ -105,22 +104,24 @@ public class TaskController {
         SortedList<Task> sortedList = new SortedList<Task>(listOfTasks, new Comparator<Task>() {
             @Override
             public int compare(Task task1, Task task2) {
-                return(task1.getEndDate().compareTo(task2.getEndDate()));
+                return (task1.getEndDate().compareTo(task2.getEndDate()));
             }
         });
 
+        tasksView.setItems(sortedList);
+        tasksView.getSelectionModel().selectFirst();
+    }
+
     @FXML
-    public void changeSceneToAddTask() throws IOException{
+    public void changeSceneToAddTask() throws IOException {
         CiterClient.setRoot("addTask");
     }
 
     /**
-     *
-     * @param task1
-     * Fills information area of Task with its information
+     * @param task1 Fills information area of Task with its information
      */
 
-    private void fillInformationArea(Task task1){
+    private void fillInformationArea(Task task1) {
         taskNameTextField.setText(task1.getTaskName());
         startDateTextField.setText(task1.getStartDate().toString());
         endDateTextField.setText(task1.getEndDate().toString());
@@ -128,7 +129,7 @@ public class TaskController {
         notesTextArea.setText(task1.getDescription());
     }
 
-    private void clearText(){
+    private void clearText() {
         for (int i = 0; i < textfieldList.size(); i++) {
             textfieldList.get(i).setText(null);
         }
@@ -136,10 +137,10 @@ public class TaskController {
     }
 
     @FXML
-    public void handleKeyPressed(KeyEvent e){
+    public void handleKeyPressed(KeyEvent e) {
         Task taskSelected = tasksView.getSelectionModel().getSelectedItem();
-        if(taskSelected != null){
-            if(e.getCode().equals(e)){
+        if (taskSelected != null) {
+            if (e.getCode().equals(e)) {
                 deleteTask(taskSelected);
             }
         }
@@ -147,27 +148,24 @@ public class TaskController {
 
     @FXML
     public void handleCategory() throws IOException {
-       CiterClient.setRoot("newCategory");
+        CiterClient.setRoot("newCategory");
     }
+
     /**
-     *
-     * @param delete
-     * Handles deletebutton
+     * @param delete Handles deletebutton
      */
     @FXML
-    public void handleDeleteButton(ActionEvent delete){
+    public void handleDeleteButton(ActionEvent delete) {
         Task selectedTask = tasksView.getSelectionModel().getSelectedItem();
-        if(selectedTask != null){
-            if(delete.getSource().equals(delete_task_button)){
+        if (selectedTask != null) {
+            if (delete.getSource().equals(delete_task_button)) {
                 deleteTask(selectedTask);
             }
         }
     }
 
     /**
-     *
-     * @param task
-     * Deletes a task with method from FileHandler class
+     * @param task Deletes a task with method from FileHandler class
      */
     public void deleteTask(Task task) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
