eee7d530b845f08a88061d1a73beb2930ef0a6b8
Henrik Burmann <henriabu@stud.ntnu.no>
2021-04-20 17:13:37 +0200

    Help menu now looks slightly less ugly

--- src/main/java/idatt1002_2021_k1_08/HelpController.java
+++ src/main/java/idatt1002_2021_k1_08/HelpController.java
@@ -2,7 +2,12 @@ package idatt1002_2021_k1_08;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class HelpController {
@@ -10,6 +15,17 @@ public class HelpController {
      * Button to return from fxml scene into main scene
      */
     @FXML Button getBackButton;
+    @FXML Label helpLabel;
+    @FXML ImageView imageView;
+    Image logo = new Image(new FileInputStream("images/CiterLogo.png"));
+
+    public HelpController() throws FileNotFoundException {
+    }
+
+    public void initialize(){
+        imageView.setImage(logo);
+
+    }
 
     /**
      * Changes scene from help to primary through fxml roots
